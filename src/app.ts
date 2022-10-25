d7aa58b6ee80838a0380e9d24e5a8408bf22eea4
WaleedOmar87 <waleedomar87@gmail.com>
2022-10-25 16:18:48 +0200

    updated config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import database from "../database/database";
+import database from "./database/database";
 
 /* Require routes */
 const ownerRoutes = require("./routes/admin");
 const customerRoutes = require("./routes/shop");
 const restaurantRoutes = require("./routes/auth");
-const reviewRoutes = require("src/routes/review");
-const reservationRoute = require("src/routes/reservation");
+const reviewRoutes = require("routes/review");
+const reservationRoute = require("routes/reservation");
 
 /* Init application */
 const app = express();
